b34b92e0fbc16d4820c4e3f5799ffc7a19468f0e
Jonathan <jonathanalonsom@outlook.com>
2025-12-20 18:14:13 +0000

    Adc na busca, companhia, preço, duracao do voo, paradas e trajeto

--- main.py
+++ main.py
@@ -4,12 +4,40 @@ flights = search_flights(
     origin="OPO",
     destination="RIO",
     departure_date="2026-03-14",
-    return_date=None,
+    return_date="2026-04-01",
     adults=1
 )
 
-if flights:
-    prices = [float(f["price"]["total"]) for f in flights]
-    print("Preço atual mais barato:", min(prices), "BRL")
-else:
+if not flights:
     print("Nenhum voo encontrado.")
+    exit()
+
+ALLOWED_AIRLINES = {"TP", "AD", "IB"}  # TAP Air Portugal, Azul, Iberia , 
+
+filtered_flights = [
+    f for f in flights
+    if any(code in ALLOWED_AIRLINES for code in f["validatingAirlineCodes"])
+]
+
+for i, flight in enumerate(flights[:5], start=1):
+    price = flight["price"]["total"]
+    currency = flight["price"]["currency"]
+    airlines = flight["validatingAirlineCodes"]
+
+    itinerary = flight["itineraries"][0]
+    segments = itinerary["segments"]
+    stops = len(segments) - 1
+    duration = itinerary["duration"]
+
+    print(f"\n✈️ VOO {i}")
+    print("Companhia(s):", airlines)
+    print("Preço:", price, currency)
+    print("Duração:", duration)
+    print("Paradas:", stops)
+
+    for seg in segments:
+        print(
+            f"  {seg['departure']['iataCode']} → "
+            f"{seg['arrival']['iataCode']} | "
+            f"{seg['carrierCode']}{seg['number']}"
+        )
